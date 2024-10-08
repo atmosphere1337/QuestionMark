44b43126073b46bcb0bc100fefeaf59969db77f3
atmosphere1337 <agbdlcider@gmail.com>
2024-10-09 03:47:22 +0800

    3 | Update

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,49 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"html"
 	"net/http"
 )
 
+// now
+// github create repo
+// single branch master
+// 5 CRUD endpoints country + entity. Do they have entities in vainla go?
+// 5 CRUD endpoints city + entity
+// 5 CRUD endpoints citizen + entity
+// dummy responses.
+// relations?
+// connect with database, postgresql?
+// elasticsearch? ES doesn't have native web interface.
+// simple frontend NEXT.js
+// end
+
+type country struct {
+	id   int
+	name string
+}
+
+type city struct {
+	id         int
+	country_id int
+	name       string
+}
+
+type citizen struct {
+	id      int
+	city_id int
+	name    string
+}
+
 func handleFnc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	c := country{name: "Russia", id: 1}
+	// fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	resp, err := json.MarshalIndent(c, "", "\n")
+	if err != nil {
+		fmt.Fprintln(w, "error")
+	}
+	fmt.Fprintln(w, "%s", resp)
 
 }
 
