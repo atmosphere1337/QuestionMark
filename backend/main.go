173510777bb26c51a115902ee8e52e12acfdcfd3
atmosphere1337 <agbdlcider@gmail.com>
2024-10-12 04:01:19 +0800

    8 | why

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
 
+// https://github.com/lib/pq/blob/master/README.md
 // db orm?
 // response format?
 
@@ -58,10 +63,35 @@ func getCountries(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, string(resp))
 }
 
+func handleDatabase(w http.ResponseWriter, r *http.Request) {
+	connStr := "user= dbname= "
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rows, err := db.Query("SELECT * FROM countries")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var country Country
+	for rows.Next() {
+		rows.Scan(&country)
+		result, err := json.Marshal(country)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Fprintln(w, string(result))
+	}
+}
+
 func main() {
 	http.HandleFunc("GET /api/v1/country", getCountry)
 	http.HandleFunc("GET /api/v1/countries", getCountries)
 	http.HandleFunc("POST /api/v1/country", createCountry)
+	http.HandleFunc("GET /api/v1/db", handleDatabase)
 
 	http.ListenAndServe(":8080", nil)
 }
